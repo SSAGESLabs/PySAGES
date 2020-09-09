2352bc9bc5e581931ed327d0b98898e822bc1378
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2020-09-08 21:29:00 -0500

    Precompute the grid cell size

--- pysages/grids.py
+++ pysages/grids.py
@@ -3,18 +3,34 @@
 
 
 from collections import namedtuple
+
+import jax.numpy as np
+from jax import jit
+from jax.lax import convert_element_type
+
 from .utils import register_pytree_namedtuple
 
 
-GridInfo = namedtuple("GridInfo", ["shape", "lower", "upper", "periodicity"])
+GridInfo = namedtuple("GridInfo", ["lower", "upper", "shape", "periodicity", "Δ"])
 
 
 @register_pytree_namedtuple
 class Grid(GridInfo):
-    def __new__(cls, shape, lower, upper, periodicity):
+    def __new__(cls, lower, upper, shape, periodicity, Δ = None):
         if not len(shape) == len(lower) == len(upper) == len(periodicity):
             raise ValueError("All arguments must be of the same lenght.")
-        return super(Grid, cls).__new__(cls, shape, lower, upper, periodicity)
+        lower = np.asarray(lower)
+        upper = np.asarray(upper)
+        shape = np.asarray(shape)
+        periodicity = np.asarray(periodicity)
+        Δ = np.divide(upper - lower, shape)
+        return super(Grid, cls).__new__(cls, lower, upper, shape, periodicity, Δ)
     #
     def __repr__(self):
         return repr("PySAGES " + type(self).__name__)
+
+
+@jit
+def get_index(grid, x):
+    I = np.floor_divide(x - grid.lower, grid.Δ)
+    return tuple(convert_element_type(I, np.uint32))
