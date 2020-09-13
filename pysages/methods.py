96aa103569eef1f8b02286355c1fedbcf16662c9
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2020-09-12 22:51:34 -0500

    Factor-out general update and checks

--- pysages/methods.py
+++ pysages/methods.py
@@ -15,12 +15,12 @@ from .utils import register_pytree_namedtuple
 
 ABFData = namedtuple(
     "ABFData",
-    ["snapshot", "bias", "grid", "hist", "Fsum", "F", "Wp", "Wp_", "N", "dt"]
+    ["bias", "hist", "Fsum", "F", "Wp", "Wp_"]
 )
 
 FUNNData = namedtuple(
     "FUNNData",
-    ["snapshot", "bias", "grid", "nn", "hist", "Fsum", "F", "Wp", "Wp_", "N", "dt"]
+    ["bias", "nn", "hist", "Fsum", "F", "Wp", "Wp_"]
 )
 
 
@@ -36,80 +36,98 @@ class FUNNState(FUNNData):
         return repr("PySAGES " + type(self).__name__)
 
 
-def abf(snapshot, grid, cv, N = 200):
-    N = np.asarray(N)
+def check_dims(grid, cv):
+    if grid.shape.size != cv.dims:
+        raise ValueError("Grid and Collective Variable dimensions must match.")
+
+
+def generic_update(cv, concrete_update):
+    _update = jit(concrete_update)
     #
-    def initialize():
-        bias = np.zeros_like(snapshot.forces)
-        hist = np.zeros(grid.shape, dtype = np.uint32)
-        Fsum = np.zeros(grid.shape)
-        F = np.zeros(cv.dims)
-        Wp = np.zeros(cv.dims)
-        Wp_ = np.zeros(cv.dims)
-        dt = snapshot.dt
-        return ABFState(snapshot, bias, grid, hist, Fsum, F, Wp, Wp_, N, dt)
-    #
-    def update(state, timestep):
-        snapshot, bias, grid, hist, Fsum, F, Wp_, Wp__, N, dt = state
-        M = snapshot.vel_mass[:, 3:4]
+    def update(snapshot, state):
+        M = snapshot.vel_mass[:, 3:]
         V = snapshot.vel_mass
         # Compute the collective variable
         ξ, Jξ = cv.ξ(snapshot.positions, snapshot.tags)
-        # Compute momenta
-        p = np.multiply(M, V).flatten()
-        # The following could equivalently be computed by np.pinv(Jξ.transpose()) @ p,
-        # but it does not work when ξ is scalar.
-        Wp = linalg.tensorsolve(Jξ @ Jξ.transpose(), Jξ @ p)
-        # Second order backward finite difference
-        dWp_dt = (1.5 * Wp - 2 * Wp_ + 0.5 * Wp__) / dt + F
         #
-        I = get_index(grid, ξ)
-        hist = hist.at[I].add(1)
-        Fsum = Fsum.at[I].add(dWp_dt)
-        n = hist[I]
-        F = dWp_dt / np.maximum(n, N)
-        #
-        bias = np.reshape(-Jξ.transpose() @ F, snapshot.forces.shape)
-        #
-        return ABFState(snapshot, bias, grid, hist, Fsum, F, Wp, Wp_, N, dt)
+        return _update(M, V, ξ, Jξ, state)
     #
-    return jit(initialize), jit(update)
+    return update
 
 
-def funn(snapshot, grid, topology, cv, N = 200):
+def abf(snapshot, grid, cv, N = 200):
+    check_dims(grid, cv)
+    #
     N = np.asarray(N)
+    dt = snapshot.dt
     #
     def initialize():
         bias = np.zeros_like(snapshot.forces)
         hist = np.zeros(grid.shape, dtype = np.uint32)
-        Fsum = np.zeros(grid.shape)
+        Fsum = np.zeros(np.hstack([grid.shape, cv.dims]))
+        F = np.zeros(cv.dims)
+        Wp = np.zeros(cv.dims)
+        Wp_ = np.zeros(cv.dims)
+        return ABFState(bias, hist, Fsum, F, Wp, Wp_)
+    #
+    def update(M, V, ξ, Jξ, state):
+        # Compute momenta
+        p = np.multiply(M, V).flatten()
+        # The following could equivalently be computed as `linalg.pinv(Jξ.T) @ p`
+        # (both seem to have the same performance).
+        Wp = linalg.tensorsolve(Jξ @ Jξ.T, Jξ @ p)
+        # Second order backward finite difference
+        dWp_dt = (1.5 * Wp - 2.0 * state.Wp + 0.5 * state.Wp_) / dt + state.F
+        #
+        I = get_index(grid, ξ)
+        H_I = state.hist[I] + 1
+        ΣF_I = state.Fsum[I] + dWp_dt
+        hist = state.hist.at[I].set(H_I)
+        Fsum = state.Fsum.at[I].set(ΣF_I)
+        F = ΣF_I / np.maximum(H_I, N)
+        #
+        bias = np.reshape(-Jξ.T @ F, snapshot.forces.shape)
+        #
+        return ABFState(bias, hist, Fsum, F, Wp, state.Wp)
+    #
+    return initialize, generic_update(cv.ξ, update)
+
+
+def funn(snapshot, grid, cv, topology, N = 200):
+    check_dims(grid, cv)
+    #
+    N = np.asarray(N)
+    dt = snapshot.dt
+    #
+    def initialize():
+        bias = np.zeros_like(snapshot.forces)
+        hist = np.zeros(grid.shape, dtype = np.uint32)
+        Fsum = np.zeros(np.hstack([grid.shape, cv.dims]))
         F = np.zeros(cv.dims)
         Wp = np.zeros(cv.dims)
         Wp_ = np.zeros(cv.dims)
         nn = neural_network(topology)
-        dt = snapshot.dt
-        return FUNNState(snapshot, bias, grid, nn, hist, Fsum, F, Wp, Wp_, N, dt)
+        return FUNNState(bias, nn, hist, Fsum, F, Wp, Wp_)
     #
-    def update(state, ξ, Jξ):
-        snapshot, grid, hist, Fsum, F, Wp_, Wp__, N, dt = state
-        M = snapshot.vel_mass[:, 3:4]
-        V = snapshot.vel_mass
+    def update(M, V, ξ, Jξ, state):
         #
         nn.train()
         Q = nn.predict(Fsum)
         #
+        # Compute momenta
         p = np.multiply(M, V).flatten()
-        Wp = linalg.tensorsolve(jξ @ Jξ.transpose(), Jξ @ p)
-        dWp_dt = (1.5 * Wp - 2 * Wp_ + 0.5 * Wp__) / dt + F + Q
+        Wp = linalg.tensorsolve(Jξ @ Jξ.T, Jξ @ p)
+        dWp_dt = (1.5 * Wp - 2.0 * state.Wp_ + 0.5 * state.Wp_) / dt + state.F + Q
         #
         I = get_index(grid, ξ)
-        hist = hist.at[I].add(1)
-        Fsum = Fsum.at[I].add(dWp_dt)
-        n = hist[I]
-        F = dWp_dt / np.maximum(n, N)
+        H_I = state.hist[I] + 1
+        ΣF_I = state.Fsum[I] + dWp_dt
+        hist = state.hist.at[I].set(H_I)
+        Fsum = state.Fsum.at[I].set(ΣF_I)
+        F = ΣF_I / np.maximum(H_I, N)
         #
-        bias = -Jξ.transpose() @ (F / np.max(n, N))
+        bias = np.reshape(-Jξ.T @ F, snapshot.forces.shape)
         #
-        return FUNNState(snapshot, bias, grid, nn, hist, Fsum, F, Wp, Wp_, N, dt)
+        return FUNNState(bias, nn, hist, Fsum, F, Wp, Wp_)
     #
-    return jit(initialize), jit(update)
+    return initialize, generic_update(cv, update)
