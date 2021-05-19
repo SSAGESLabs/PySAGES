4567d764dfc6b18e5e8e46e136f67f8ea35732a2
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-05-19 16:09:10 -0500

    Gitignore _version.py
    and do not export version from it

--- pysages/__init__.py
+++ pysages/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020: Pablo Zubieta (see LICENSE.md)
 
 from ._version import version as __version__
-from ._version import version, version_tuple
+from ._version import version_tuple
 
 from .backends import (
     bind,
