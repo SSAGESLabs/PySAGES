e16b4c8f70905356012f519bd7efa4fa799c5841
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-05-24 00:50:51 -0500

    Add spectral approximation submodule

---
+++ pysages/approxfun/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020-2021: PySAGES contributors
+# See LICENSE.md and CONTRIBUTORS.md at https://github.com/SSAGESLabs/PySAGES
+
+# pylint: disable=unused-import,relative-beyond-top-level
+
+from .core import (
+    VandermondeGradientFit,
+    build_fitter,
+    build_interpolator,
+    build_integrator,
+    collect_exponents,
+    compute_mesh,
+)
