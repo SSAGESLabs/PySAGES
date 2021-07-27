b1d1daf979ed530e103129559ac36ba384c372ed
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-07-27 00:09:06 -0500

    Organize parameters packing/unpacking info

--- pysages/nn/utils.py
+++ pysages/nn/utils.py
@@ -2,10 +2,21 @@
 # Copyright (c) 2020-2021: PySAGES contributors
 # See LICENSE.md and CONTRIBUTORS.md at https://github.com/SSAGESLabs/PySAGES
 
+from jaxlib.xla_extension import DeviceArray, PyTreeDef
+from typing import NamedTuple
 
 import jax
-
 import jax.numpy as np
+
+
+class ParametersLayout(NamedTuple):
+    """
+    Holds the information needed to pack flatten parameters of a
+    `jax.experimental.stax.serial` model.
+    """
+    structure:  PyTreeDef
+    shapes:     list
+    separators: DeviceArray
 
 
 def rng_key(seed=0, n=2):
@@ -29,29 +40,29 @@ def prod(xs):
 # %% Models
 def unpack(params):
     data, structure = jax.tree_flatten(params)
-    shapes = [entry.shape for entry in data]
-    s = (prod(shapes[-1]), -1)
-    ps = np.hstack([np.reshape(entry, s) for entry in data])
-    return ps, shapes, structure
+    ps = np.hstack([values.flatten() for values in data])
+    shapes = [values.shape for values in data]
+    separators = np.cumsum(np.array([prod(s) for s in shapes[:-1]]))
+    return ps, ParametersLayout(structure, shapes, separators)
 
 
-def pack(structure, shapes, params):
-    inds = np.cumsum(np.array([prod(s) for s in shapes]))
-    part = np.split(params, inds[:-1], axis=1)
-    ps = [p.reshape(s) for (p, s) in zip(part, shapes)]
+def pack(params, layout):
+    structure, shapes, separators = layout
+    partition = params.split(separators)
+    ps = [p.reshape(s) for (p, s) in zip(partition, shapes)]
     return structure.unflatten(ps)
 
 
 # %% Objectives, Costs, Regularization
 def v_error(model):
-    _, shapes, structure = unpack(model.parameters)
-    #
-    def compute(p, inputs, reference):
+    _, layout = unpack(model.parameters)
+
+    def compute(ps, inputs, reference):
         "Vectorized (pointwise) error"
-        params = pack(structure, shapes, p)
+        params = pack(ps, layout)
         prediction = model.apply(params, inputs)
         return np.float32(prediction - reference)
-    #
+
     return compute
 
 
