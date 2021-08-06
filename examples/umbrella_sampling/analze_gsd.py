69e210e1a9386b37b1cdc45caf549878de89d85e
Ludwig Schneider <ludwigschneider@uchicago.edu>
2021-08-06 14:57:02 -0500

    adding umbrella sampling test

---
+++ examples/umbrella_sampling/analze_gsd.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import sys
+
+import gsd
+import gsd.hoomd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_positions(traj):
+    pos = np.zeros((len(traj), traj[0].particles.N, 3))
+    for (i, snap) in zip(range(len(traj)), traj):
+        pos[i] = snap.particles.position
+    return pos
+
+
+def get_histogram(pos, idx, N=25):
+    hist = np.zeros((3, N))
+    for i in range(3):
+        hist[i], _ = np.histogram(pos[:,idx, i], N, density=True)
+    return hist
+
+def plot(unbiased_hist, biased_hist):
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel(r"coordinate $X_\alpha/L$")
+    ax.set_ylabel(r"$p(X_\alpha)$")
+
+    x = np.linspace(0, 1, unbiased_hist.shape[1])
+    ax.plot(x, unbiased_hist[0], label="unbiased X")
+    ax.plot(x, unbiased_hist[1], label="unbiased Y")
+    ax.plot(x, unbiased_hist[2], label="unbiased Z")
+
+    ax.plot(x, biased_hist[0], label="biased X")
+    ax.plot(x, biased_hist[1], label="biased Y")
+    ax.plot(x, biased_hist[2], label="biased Z")
+
+    ax.legend(loc="best")
+    fig.savefig("hist.pdf")
+    plt.close(fig)
+
+
+def main(argv):
+    if len(argv) != 1:
+        print("Usage: ./analyze_gsd.py filename")
+        return
+    filename = argv[0]
+
+    with gsd.hoomd.open(filename, "rb") as traj:
+        pos = get_positions(traj)
+    biased_hist = get_histogram(pos, 0)
+    unbiased_hist = get_histogram(pos, 1)
+
+    plot(unbiased_hist, biased_hist)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ examples/umbrella_sampling/umbrella.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import h5py
+import numpy as np
+
+import hoomd
+hoomd.context.initialize()
+import hoomd.md as md
+import hoomd.dlext
+
+import pysages
+from pysages.ssages.collective_variables import Component
+from pysages.ssages.methods import UmbrellaSampling
+from pysages.backends import bind
+
+def main():
+    with hoomd.context.SimulationContext() as context:
+        system = hoomd.init.read_gsd("start.gsd")
+
+        #gather basic lognames
+        qr = []
+        #log some thermo properties
+        qr += ['temperature', 'potential_energy', 'kinetic_energy']
+
+        cvs = (Component([0], 0), Component([0], 1))
+
+        method = UmbrellaSampling(cvs, 10., (-2, 2))
+
+        bind(context, method)
+
+        hoomd.md.integrate.nvt(group=hoomd.group.all(), kT=1, tau=0.1)
+        hoomd.md.integrate.mode_standard(dt=0.05)
+
+        gsd = hoomd.dump.gsd("umbrella.gsd", group=hoomd.group.all(), period=100, overwrite=True)
+
+        hoomd.run(1e6, limit_multiple=100)
+
+
+
+if __name__ == "__main__":
+    main()
