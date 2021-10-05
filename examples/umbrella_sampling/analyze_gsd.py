d042f15989e52d4511a2741537fc589aea074399
Ludwig Schneider <ludwig.schneider@theorie.physik.uni-goettingen.de>
2021-10-05 11:40:40 -0500

    Add Umbrella Sampling (#35)
    Co-authored-by: Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>

---
+++ examples/umbrella_sampling/analyze_gsd.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+import sys
+
+import gsd
+import gsd.hoomd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_positions(traj):
+    pos = np.zeros((len(traj), traj[0].particles.N, 3))
+    for (i, snap) in zip(range(len(traj)), traj):
+        pos[i] = snap.particles.position
+    return pos
+
+
+def get_histogram(pos, idx, L, N=25):
+    hist = np.zeros((3, N))
+    for i in range(3):
+        hist[i], _ = np.histogram(pos[:,idx, i], bins=N, range=(-L[i]/2, L[i]/2), density=True)
+    return hist
+
+def plot(unbiased_hist, biased_hist):
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel(r"coordinate $X_\alpha/L$")
+    ax.set_ylabel(r"$p(X_\alpha)$")
+
+    x = np.linspace(0, 1, unbiased_hist.shape[1])
+    ax.plot(x, unbiased_hist[0], label="unbiased X")
+    ax.plot(x, unbiased_hist[1], label="unbiased Y")
+    ax.plot(x, unbiased_hist[2], label="unbiased Z")
+
+    ax.plot(x, biased_hist[0], label="biased X")
+    ax.plot(x, biased_hist[1], label="biased Y")
+    ax.plot(x, biased_hist[2], label="biased Z")
+
+    ax.legend(loc="best")
+    fig.savefig("hist.pdf")
+    plt.close(fig)
+
+
+def validate_hist(unbiased_hist, biased_hist):
+    TARGET_NORM = 0.2
+    TARGET_BIAS = [[0., 0., 0.005, 0.005, 0.005, 0.02, 0.15, 0.315, 0.745, 1.23, 1.325, 0.685, 0.365, 0.125, 0.01, 0.005, 0.005, 0.005, 0., 0., 0., 0., 0., 0., 0.   ],
+                   [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.015, 0.02, 0.15, 0.325, 0.77, 1.245, 1.155, 0.76, 0.41, 0.1, 0.04, 0.01, 0., 0., 0., 0.],
+                   [0., 0., 0., 0., 0., 0., 0., 0.005, 0.07, 0.23, 0.645, 0.9, 1.275, 0.975, 0.565, 0.295, 0.04, 0., 0., 0., 0., 0., 0., 0., 0.]]
+
+
+
+    EPSILON = 0.1
+
+    for i in range(3):
+        val = np.sqrt(np.mean((unbiased_hist[i]-TARGET_NORM)**2))
+        if val > EPSILON:
+            raise RuntimeError("Unbiased historgram deviation too large: {0} epsilon {1}".format(val, EPSILON))
+    for i in range(3):
+        val = np.sqrt(np.mean((biased_hist[i]-TARGET_BIAS[i])**2))
+        if val > EPSILON:
+            raise RuntimeError("Biased historgram deviation too large: {0} epsilon {1}".format(val, EPSILON))
+
+def main(argv):
+    if len(argv) != 1:
+        print("Usage: ./analyze_gsd.py filename")
+        return
+    filename = argv[0]
+
+    with gsd.hoomd.open(filename, "rb") as traj:
+        pos = get_positions(traj)
+        L = traj[0].configuration.box[:3]
+    biased_hist = get_histogram(pos, 0, L)
+    unbiased_hist = get_histogram(pos, 1, L)
+
+    plot(unbiased_hist, biased_hist)
+    validate_hist(unbiased_hist, biased_hist)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ examples/umbrella_sampling/umbrella.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+import hoomd
+hoomd.context.initialize()
+import hoomd.md as md
+import hoomd.dlext
+
+import pysages
+from pysages.ssages.collective_variables import Component
+from pysages.ssages.methods import UmbrellaSampling
+from pysages.backends import bind
+
+def main():
+    with hoomd.context.SimulationContext() as context:
+        system = hoomd.init.read_gsd("start.gsd")
+
+        #gather basic lognames
+        qr = []
+        #log some thermo properties
+        qr += ['temperature', 'potential_energy', 'kinetic_energy']
+
+        cvs = [Component([0], 2),]
+        cvs += [Component([0], 1),]
+        cvs += [Component([0], 0),]
+
+        center_cv = [ 0.,]
+        center_cv += [0.5, -0.5]
+
+        method = UmbrellaSampling(cvs, 10., center_cv)
+
+        hoomd.md.integrate.nve(group=hoomd.group.all())
+        hoomd.md.integrate.mode_standard(dt=0.01)
+
+        nl = hoomd.md.nlist.cell()
+        dpd = hoomd.md.pair.dpd(r_cut=1,nlist=nl,seed=42,kT=1.)
+        dpd.pair_coeff.set("A","A",A=5.,gamma=1.0)
+
+        hoomd.analyze.log("umbrella.dat", qr, 100, overwrite=True)
+
+        bind(context, method)
+
+        gsd = hoomd.dump.gsd("umbrella.gsd", group=hoomd.group.all(), period=100, overwrite=True)
+
+        hoomd.run(1e5, limit_multiple=100)
+
+
+
+if __name__ == "__main__":
+    main()
