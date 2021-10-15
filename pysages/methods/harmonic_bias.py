831ceea57fbb525c0ee8641dde127b782e820284
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-10-15 15:17:40 -0500

    Simplify set_spring logic, format docstrings

--- pysages/methods/harmonic_bias.py
+++ pysages/methods/harmonic_bias.py
@@ -12,8 +12,10 @@ class HarmonicBiasState(NamedTuple):
     """
     Description of a state bias by a harmonic potential for a CV.
 
-    bias -- additional forces for the class.
-    xi -- current cv value.
+    bias: JaxArray
+        Biasing forces of a `HarmonicBias` simulation.
+    xi: JaxArray
+        Collective variable value of the last simulation step.
     """
     bias: JaxArray
     xi: JaxArray
@@ -27,10 +29,14 @@ class HarmonicBias(SamplingMethod):
 
     def __init__(self, cvs, kspring, center, *args, **kwargs):
         """
-        cvs: list of collective variables length N
-        kspring: float, array length N or symmetric NxN matrix. Restraining spring constant.
-        center: array of length N representing the minimum of the harmonic draining potential.
-        args and kwargs for parent constructor
+        Arguments
+        ---------
+        cvs: Union[List, Tuple]
+            A list or Tuple of collective variables, length `N`.
+        kspring:
+            A scalar, array length `N` or symmetric `N x N` matrix. Restraining spring constant.
+        center:
+            An array of length `N` representing the minimum of the harmonic biasing potential.
         """
         super().__init__(cvs, args, kwargs)
         self._N = len(cvs)
@@ -40,27 +46,32 @@ class HarmonicBias(SamplingMethod):
     def set_kspring(self, kspring):
         """
         Set new spring constant.
-        kspring: float, array length N or symmetric NxN matrix. Restraining spring constant.
+
+        Arguments
+        ---------
+        kspring:
+            A scalar, array length `N` or symmetric `N x N` matrix. Restraining spring constant.
         """
-
-        # ensure array
+        # Ensure array
         kspring = np.asarray(kspring)
-        if len(kspring.shape) == 2:
-            if kspring.shape != (self._N, self._N):
-                raise RuntimeError("2D kspring with wrong shape, expected {0}x{0} got {1}".format(self._N, kspring.shape))
+        shape = kspring.shape
+        N = self._N
+
+        if len(shape) > 2:
+            raise RuntimeError(f"Wrong kspring shape {shape} (expected scalar, 1D or 2D)")
+        elif len(shape) == 2:
+            if shape != (N, N):
+                raise RuntimeError(f"2D kspring with wrong shape, expected ({N}, {N}), got {shape}")
+            if not np.allclose(kspring, kspring.T):
+                raise RuntimeError("Spring matrix is not symmetric")
+            
             self._kspring = kspring
-        else:
-            if len(kspring.shape) == 0:
-                kspring = np.ones(self._N) * kspring
-            if len(kspring.shape) !=1:
-                raise RuntimeError("Wrong shape of kspring {0}, expected 1D or 2D".format(kspring.shape))
-            if not (kspring.shape[0] == self._N or kspring.shape[0] == 1) :
-                raise RuntimeError("Length of 1D kspring unequal with the length of collective variables. {0} {1}".format(kspring.shape, self._N))
+        else:  # len(shape) == 0 or len(shape) == 1
+            n = kspring.size
+            if n != N and n != 1:
+                raise RuntimeError(f"Wrong kspring size, expected 1 or {N}, got {n}.")
 
-            self._kspring = np.identity(self._N) * kspring
-
-        if not np.allclose(self._kspring, self._kspring.T):
-            raise RuntimeError("Spring matrix is not symmetric")
+            self._kspring = np.identity(N) * kspring
 
         return self._kspring
 
@@ -70,9 +81,8 @@ class HarmonicBias(SamplingMethod):
     def set_center(self, center):
         center = np.asarray(center)
         if len(center.shape) !=1 or center.shape[0] != self._N:
-            raise RuntimeError("Invalid center shape expected {0} got {1}.".format(self._N, center.shape))
+            raise RuntimeError(f"Invalid center shape expected {self._N} got {center.shape}.")
         self._center = center
-
 
     def get_center(self):
         return self._center
