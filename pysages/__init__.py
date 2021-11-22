f7333bb0b1406105db06dc571c9d8305ce926101
Ludwig Schneider <ludwigschneider@uchicago.edu>
2021-11-22 17:03:48 -0600

    Manually switch off XLA/GPU memory preallocation

--- pysages/__init__.py
+++ pysages/__init__.py
@@ -2,6 +2,15 @@
 # Copyright (c) 2020-2021: PySAGES contributors
 # See LICENSE.md and CONTRIBUTORS.md at https://github.com/SSAGESLabs/PySAGES
 
+import os
+# Check for user set memory environment for XLA/JAX
+if not ("XLA_PYTHON_CLIENT_PREALLOCATE" in os.environ
+        or "XLA_PYTHON_CLIENT_MEM_FRACTION" in os.environ
+        or "XLA_PYTHON_CLIENT_ALLOCATOR" in os.environ):
+    # If not set be user, disable preallocate to enable multiple/growing
+    # simulation memory footprints
+    os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+
 from ._version import version as __version__
 from ._version import version_tuple
 
