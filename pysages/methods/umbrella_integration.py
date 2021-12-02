f6669317f0cc425c780a412e5ea0802e46caff6c
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-12-02 15:48:28 -0600

    Some docs and code clean-up

--- pysages/methods/umbrella_integration.py
+++ pysages/methods/umbrella_integration.py
@@ -2,11 +2,13 @@
 # Copyright (c) 2020-2021: PySAGES contributors
 # See LICENSE.md and CONTRIBUTORS.md at https://github.com/SSAGESLabs/PySAGES
 
-import jax.numpy as np
 from typing import Callable
+
 from pysages.backends import ContextWrapper
-from .harmonic_bias import HarmonicBias
-from .utils import HistogramLogger
+from pysages.methods.harmonic_bias import HarmonicBias
+from pysages.methods.utils import HistogramLogger
+
+import jax.numpy as np
 
 
 class UmbrellaIntegration(HarmonicBias):
@@ -14,31 +16,60 @@ class UmbrellaIntegration(HarmonicBias):
         kspring = center = np.zeros(len(cvs))
         super().__init__(cvs, kspring, center, args, kwargs)
 
-    def run(self,
-            context_generator: Callable,
-            timesteps : int,
-            centers,
-            ksprings,
-            hist_periods,
-            hist_offsets = 0,
-            context_args=dict(),
-            **kwargs):
+    def run(
+        self,
+        context_generator: Callable,
+        timesteps: int,
+        centers,
+        ksprings,
+        hist_periods,
+        hist_offsets = 0,
+        context_args = dict(),
+        **kwargs
+    ):
         """
-        Implementation of the serial execution of umbrella integration with up to linear order (ignoring second order terms with covariance matrix) as described in
-        J. Chem. Phys. 131, 034109 (2009); https://doi.org/10.1063/1.3175798 Equation 13. Higher order approximations can be implemented by the user using the provided covariance matrix.
+        Implementation of the serial execution of umbrella integration
+        with up to linear order (ignoring second order terms with covariance matrix)
+        as described in J. Chem. Phys. 131, 034109 (2009); https://doi.org/10.1063/1.3175798 (equation 13).
+        Higher order approximations can be implemented by the user using the provided covariance matrix.
 
-        context_generator: user defined function that sets up a simulation context with the backend.
-                           Must return an instance of hoomd.conext.SimulationContext for hoomd-blue and simtk.openmm.Context.
-                           The function gets context_args unpacked for additional user args.
-                           For each replica along the path, the argument "replica_num" i [0, ..., N-1] is set for the context_generator to load the appropriate initial condition.
-        timesteps: number of timesteps the simulation is running.
-        centers: list of CV centers along the path of integration. The length defines the number replicas.
-        ksprings: float or list of floats describing the spring strength of the harmonic bias for each replica.
-        hist_periods: int or list of int describing the period for the histrogram logging of each replica.
-        hist_offsets: int or list of int describing the offset before starting the histogram of each replica.
-        kwargs: gets passed to the backend run function for additional user arguments to be passed down.
-        User defined callback are not available, the method requires use of a builtin callback.
+        Arguments
+        ---------
+        context_generator: Callable
+            User defined function that sets up a simulation context with the backend.
+            Must return an instance of hoomd.conext.SimulationContext for HOOMD-blue and openmm.Context for OpenMM.
+            The function gets `context_args` unpacked for additional user args.
+            For each replica along the path, the argument `replica_num` in [0, ..., N-1]
+            is set in the `context_generator` to load the appropriate initial condition.
+
+        timesteps: int
+            Number of timesteps the simulation is running.
+
+        centers: list[numbers.Real]
+            CV centers along the path of integration. The length defines the number replicas.
+
+        ksprings: Union[float, list[float]]
+            Spring constants of the harmonic biasing potential for each replica.
+
+        hist_periods: Union[int, list[int]]
+            Describes the period for the histrogram logging of each replica.
+
+        hist_offsets: Union[int, list[int]]
+            Offset applied before starting the histogram of each replica.
+
+        kwargs:
+            Passed to the backend run function as additional user arguments.
+
+        * Note:
+            This method does not accepts a user defined callback are not available.
         """
+
+        def free_energy_gradient(K, mean, center, i):
+            "Equation 13 from https://doi.org/10.1063/1.3175798"
+            return -(K @ (mean - center))
+
+        def integrate(A, nabla_A, centers, i):
+            return A[i-1] + nabla_A[i-1].T @ (centers[i] - centers[i-1])
 
         def collect(arg, Nreplica, name, dtype):
             if isinstance(arg, list):
@@ -64,27 +95,28 @@ class UmbrellaIntegration(HarmonicBias):
         result["A"] = []
 
         for rep in range(Nreplica):
-            context_args["replica_num"] = rep
             self.center = centers[rep]
             self.kspring = ksprings[rep]
-            callback = HistogramLogger(hist_periods[rep], hist_offsets[rep])
+
+            context_args["replica_num"] = rep
             context = context_generator(**context_args)
+            callback = HistogramLogger(hist_periods[rep], hist_offsets[rep])
             wrapped_context = ContextWrapper(context, self, callback)
+
             with wrapped_context:
                 wrapped_context.run(timesteps[rep])
 
+            mean = callback.get_means()
+
             result["kspring"].append(self.kspring)
             result["center"].append(self.center)
-
             result["histogram"].append(callback)
-            result["histogram_means"].append(callback.get_means())
-
-            # Equation 13
-            result["nabla_A"].append(-result["kspring"][rep] @ (result["histogram_means"][rep] - result["center"][rep]))
-            # discrete forward integration of the free-energy
+            result["histogram_means"].append(mean)
+            result["nabla_A"].append(free_energy_gradient(self.kspring, mean, self.center))
+            # Discrete forward integration of the free-energy
             if rep == 0:
                 result["A"].append(0)
             else:
-                result["A"].append( result["A"][rep-1] + result["nabla_A"][rep-1].T @ (result["center"][rep] - result["center"][rep-1]))
+                result["A"].append(integrate(result["A"], result["nabla_A"], result["center"], rep))
 
         return result
