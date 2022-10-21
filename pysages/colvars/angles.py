b6f5cd0b64a9b1ebec2e4588829851508f96c828
Riccardo Alessandri <ric.alessandri@gmail.com>
2022-10-21 11:06:11 -0500

    Update pysages/colvars/angles.py: clarify conversion to AP

--- pysages/colvars/angles.py
+++ pysages/colvars/angles.py
@@ -146,7 +146,7 @@ class RingPuckeringCoordinates(CollectiveVariable):
     to the Altona-Sundaralingam order parameter by adding `pi / 2` to the result
     and then converting from radians to degrees.
     Similarly, the amplitude obtained via the Cremer-Pople method can be converted
-    to the Altona-Sundaralingam order parameter by multiplying the result by `1025`.
+    to the Altona-Sundaralingam order parameter (in degrees) by multiplying the result by `1025` degree/nm.
     (assume the unit is nanometer for the amplitude, and this will convert from nanometer to degree)
 
     Usage
