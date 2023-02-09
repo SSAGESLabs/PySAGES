f20e928d8ea6f70aa341aed970f45f7e8d792715
Trung Nguyen <ndactrung@gmail.com>
2023-02-09 17:29:06 -0600

    Added examples for lammps backend, first try unbiased

---
+++ examples/lammps/unbiased/unbiased.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import lammps
+#import lammps.dlext
+from lammps import lammps
+import numpy as np
+
+import pysages
+from pysages.colvars import Component
+from pysages.methods import HistogramLogger, Unbiased
+
+
+def generate_context(**kwargs):
+
+    args="-k on g 1 -sf kk"
+    args=args.split()
+    #args=["-k", "on", "g", "1", "-sf", "kk"]
+    context = lammps(cmdargs=args)
+
+    # run infile one line at a time
+    lines = open(infile,'r').readlines()
+    for line in lines: context.command(line)
+    return context
+
+
+def main():
+    cvs = [Component([0], 2)]
+    cvs += [Component([0], 1)]
+    cvs += [Component([0], 0)]
+
+    center_cv = [0.0]
+    center_cv += [1.0, -0.3]
+
+    method = Unbiased(cvs)
+    callback = HistogramLogger(10)
+
+    #raw_result = pysages.run(method, generate_context, int(1e2), callback)
+
+    #print(np.asarray(raw_result.callbacks[0].data))
+
+
+if __name__ == "__main__":
+    main()
