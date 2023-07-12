38fc9174841232db72419128ef8d70f623a98441
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2023-07-12 10:13:19 -0600

    Update again the lammps backend interface

--- pysages/backends/lammps.py
+++ pysages/backends/lammps.py
@@ -5,18 +5,23 @@
 
 import importlib
 from functools import partial
-from typing import Callable
 
+import jax
+from jax import jit, vmap
+from jax import numpy as np
+from jax.dlpack import from_dlpack as asarray
 from lammps import dlext
 from lammps.dlext import (
     ExecutionSpace,
     FixDLExt,
-    LAMMPSView,
+    kImgBitSize,
+    kImgBits,
+    kImg2Bits,
+    kImgMask,
+    kImgMax,
 )
-from jax import jit
-from jax import numpy as np
-from jax.dlpack import from_dlpack as asarray
 
+from pysages.backends import snapshot as ps
 from pysages.backends.core import SamplingContext
 from pysages.backends.snapshot import (
     Box,
@@ -25,144 +30,70 @@ from pysages.backends.snapshot import (
     SnapshotMethods,
     build_data_querier,
 )
-from pysages.backends.snapshot import restore as _restore
-from pysages.utils import check_device_array, copy
+from pysages.typing import Callable, Optional
+from pysages.utils import copy
+
+kDefaultLocation = (
+    ExecutionSpace.kOnHost if not hasattr(ExecutionSpace, "kOnDevice") else ExecutionSpace.kOnDevice
+)
 
 
-def get_dimension(context):
-    return context.extract_setting("dimension")
-
-
-def get_timestep(context):
-    return context.extract_global("dt")
-
-
-def get_global_box(context):
-    boxlo, boxhi, xy, yz, xz, *_ = context.extract_box()
-    Lx = boxhi[0] - boxlo[0]
-    Ly = boxhi[1] - boxlo[1]
-    Lz = boxhi[2] - boxlo[2]
-    origin = boxlo
-    H = ((Lx, xy * Ly, xz * Lz), (0.0, Ly, yz * Lz), (0.0, 0.0, Lz))
-    return H, origin
-
-
-def set_post_force_hook(context, post_force_hook):
-    # set_fix_external_callback
-    context.set_fix_external_callback(post_force_hook)
-
-
-# DLExtSampler is exported from lammps_dlext
 class Sampler(FixDLExt):
-    def __init__(self, context, method_bundle, bias, callback: Callable, restore):
+    def __init__(
+        self, context, sampling_method, callback: Optional[Callable], location=kDefaultLocation
+    ):
         super().__init__(context, "1 all dlext space device".split())
 
-        initial_snapshot, initialize, method_update = method_bundle
+        on_gpu = (location != dlext.kOnHost) & self.view.has_kokkos_cuda_enabled()
+        helpers, restore, bias = build_helpers(context, sampling_method, on_gpu, ps.restore)
+
+        initial_snapshot = take_snapshot(context, self.view, location=location)
+        _, initialize, method_update = sampling_method.build(initial_snapshot, helpers)
+
+        self.location = location
+        self.state = initialize()
+        self.bias = partial(bias, sync_backend=self.view.synchronize)
+        self.callback = callback
+        self.snapshot = initial_snapshot
+        self.restore = lambda prev_snapshot: restore(self.snapshot, prev_snapshot)
+        self.take_snapshot = partial(take_snapshot, context, self.view)
+
+        def update_box():
+            return self.snapshot.box
+
+        def update_snapshot():
+            view = self.view
+            location = self.location
+
+            positions = asarray(dlext.positions(view, location))
+            types = asarray(dlext.types(view, location))
+            velocities = asarray(dlext.velocities(view, location))
+            forces = asarray(dlext.forces(view, location))
+            tags_map = asarray(dlext.tags_map(view, location))[1:]
+            imgs = asarray(dlext.images(view, location))
+
+            _, (masses, _) = self.snapshot.vel_mass
+            vel_mass = (velocities, (masses, types))
+            box = update_box()
+            dt = self.snapshot.dt
+
+            return Snapshot(positions, vel_mass, forces, tags_map, imgs, box, dt)
 
         def update(timestep):
+            self.snapshot = update_snapshot()
             self.state = method_update(self.snapshot, self.state)
             self.bias(self.snapshot, self.state)
             if self.callback:
                 self.callback(self.snapshot, self.state, timestep)
 
-        self.set_callback(update)
-        self.state = initialize()
-        self.bias = bias
-        self.callback = callback
-        self.snapshot = initial_snapshot
-        self.dt = initial_snapshot.dt
-        self.box = initial_snapshot.box
-        self._restore = restore
-
-    def restore(self, prev_snapshot):
-        self._restore(self.snapshot, prev_snapshot)
-
-    def take_snapshot(self):
-        location = default_location()
-
-        positions = copy(asarray(dlext.positions(self.view, location)))
-        types = copy(asarray(dlext.types(self.view, location)))
-        velocities = copy(asarray(dlext.velocities(self.view, location)))
-        masses = copy(asarray(dlext.masses(self.view, location)))
-        forces = copy(asarray(dlext.forces(self.view, location)))
-        tags = copy(asarray(dlext.tags(self.view, location)))
-        imgs = copy(asarray(dlext.images(self.view, location)))
-
-        vel_mass = (velocities, masses[types])
-
-        return Snapshot(positions, vel_mass, forces, tags, imgs, self.box, self.dt)
+        self.update = update
+        self.set_callback(self.update)
 
 
-if hasattr(ExecutionSpace, "kOnDevice"):
-
-    def default_location():
-        return ExecutionSpace.kOnDevice
-
-else:
-
-    def default_location():
-        return ExecutionSpace.kOnHost
-
-
-# build a Snapshot object that contains all the tensors from the context
-def take_snapshot(sampling_context, location=default_location()):
-    context = sampling_context.context
-    view = sampling_context.view
-
-    positions = copy(asarray(dlext.positions(view, location)))
-    types = copy(asarray(dlext.types(view, location)))
-    velocities = copy(asarray(dlext.velocities(view, location)))
-    masses = copy(asarray(dlext.masses(view, location)))
-    net_forces = copy(asarray(dlext.forces(view, location)))
-    tags = copy(asarray(dlext.tags(view, location)))
-    imgs = copy(asarray(dlext.images(view, location)))
-
-    vel_mass = (velocities, masses[types])
-
-    check_device_array(positions)  # currently, we only support `DeviceArray`s
-
-    H, origin = get_global_box(context)
-    dt = get_timestep(context)
-
-    return Snapshot(positions, vel_mass, net_forces, tags, imgs, Box(H, origin), dt)
-
-
-def build_snapshot_methods(sampling_method):
-    if sampling_method.requires_box_unwrapping:
-
-        def positions(snapshot):
-            L = np.diag(snapshot.box.H)
-            return snapshot.positions[:, :3] + L * snapshot.images
-
-    else:
-
-        def positions(snapshot):
-            return snapshot.positions
-
-    @jit
-    def indices(snapshot):
-        return snapshot.ids
-
-    @jit
-    def momenta(snapshot):
-        V, M = snapshot.vel_mass
-        return (M * V).flatten()
-
-    @jit
-    def masses(snapshot):
-        return snapshot.vel_mass[:, 3:]
-
-    return SnapshotMethods(jit(positions), indices, momenta, masses)
-
-
-def build_helpers(sampling_context):
-    context = sampling_context.context
-    sampling_method = sampling_context.method
-    view = sampling_context.view
-
+def build_helpers(context, sampling_method, on_gpu, restore_fn):
     # Depending on the device being used we need to use either cupy or numpy
     # (or numba) to generate a view of jax's DeviceArrays
-    if view.has_kokkos_cuda_enabled():
+    if on_gpu:
         cupy = importlib.import_module("cupy")
         view = cupy.asarray
 
@@ -189,29 +120,93 @@ def build_helpers(sampling_context):
         forces[:, :3] += biases
         sync_forces()
 
-    snapshot_methods = build_snapshot_methods(sampling_method)
+    snapshot_methods = build_snapshot_methods(sampling_method, on_gpu)
     flags = sampling_method.snapshot_flags
-    restore = partial(_restore, view)
+    restore = partial(restore_fn, view)
     helpers = HelperMethods(build_data_querier(snapshot_methods, flags), get_dimension(context))
 
     return helpers, restore, bias
 
 
-def bind(sampling_context: SamplingContext, callback: Callable, **kwargs):
+def build_snapshot_methods(sampling_method, on_gpu):
+    if sampling_method.requires_box_unwrapping:
+        device = jax.devices("gpu" if on_gpu else "cpu")[0]
+        dtype = np.int64 if kImgBitSize == 64 else np.int32
+        all_mask = 2 ** (kImgBitSize - 1) - 1
+        offset = kImgMax
+
+        with jax.default_device(device):
+            bits = np.asarray((0, kImgBits, kImg2Bits), dtype=dtype)
+            mask = np.asarray((kImgMask, kImgMask, all_mask), dtype=dtype)
+
+        def unpack(image):
+            return (image >> bits & mask) - offset
+
+        def positions(snapshot):
+            L = np.diag(snapshot.box.H)
+            return snapshot.positions[:, :3] + L * vmap(unpack)(snapshot.images)
+
+    else:
+
+        def positions(snapshot):
+            return snapshot.positions
+
+    @jit
+    def indices(snapshot):
+        return snapshot.ids
+
+    @jit
+    def momenta(snapshot):
+        V, (masses, types) = snapshot.vel_mass
+        M = masses[types]
+        return (M * V).flatten()
+
+    @jit
+    def masses(snapshot):
+        return snapshot.vel_mass[:, 3:]
+
+    return SnapshotMethods(jit(positions), indices, momenta, masses)
+
+
+def get_dimension(context):
+    return context.extract_setting("dimension")
+
+
+def get_global_box(context):
+    boxlo, boxhi, xy, yz, xz, *_ = context.extract_box()
+    Lx = boxhi[0] - boxlo[0]
+    Ly = boxhi[1] - boxlo[1]
+    Lz = boxhi[2] - boxlo[2]
+    origin = boxlo
+    H = ((Lx, xy * Ly, xz * Lz), (0.0, Ly, yz * Lz), (0.0, 0.0, Lz))
+    return H, origin
+
+
+def get_timestep(context):
+    return context.extract_global("dt")
+
+
+def take_snapshot(context, view, location=kDefaultLocation):
+    positions = copy(asarray(dlext.positions(view, location)))
+    types = copy(asarray(dlext.types(view, location)))
+    velocities = copy(asarray(dlext.velocities(view, location)))
+    masses = copy(asarray(dlext.masses(view, location)))
+    forces = copy(asarray(dlext.forces(view, location)))
+    tags_map = asarray(dlext.tags_map(view, location))[1:]
+    imgs = copy(asarray(dlext.images(view, location)))
+
+    vel_mass = (velocities, (masses, types))
+
+    box = Box(*get_global_box(context))
+    dt = get_timestep(context)
+
+    return Snapshot(positions, vel_mass, forces, tags_map, imgs, box, dt)
+
+
+def bind(sampling_context: SamplingContext, callback: Optional[Callable], **kwargs):
     context = sampling_context.context
     sampling_method = sampling_context.method
-    view = LAMMPSView(context)
-    sampling_context.view = view
+    sampler = Sampler(context, sampling_method, callback)
+    sampling_context.view = sampler.view
     sampling_context.run = lambda n, **kwargs: context.command(f"run {n}")
-    helpers, restore, bias = build_helpers(sampling_context)
-
-    snapshot = take_snapshot(sampling_context)
-
-    # the build() member function of a specific sampling method
-    # (e.g. see abf.py) returns a triplet:
-    #   initial_snapshot (object)  : initial configuration of the simulation context
-    #   initialize       (function): returns the initial state of the sampling method
-    #   method_update    (function): generalize() update a state depending on JIT or not
-    method_bundle = sampling_method.build(snapshot, helpers)
-    sync_and_bias = partial(bias, sync_backend=view.synchronize)
-    return Sampler(view, method_bundle, sync_and_bias, callback, restore)
+    return sampler
