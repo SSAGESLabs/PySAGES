b2792a4619fdfdd3f62b9e5cd1e58b6de93a4f83
yihengwuKP <yihengwu917@uchicago.edu>
2024-05-24 15:42:35 -0500

    fix the path issue with pytest

--- tests/test_rmsd.py
+++ tests/test_rmsd.py
@@ -4,8 +4,8 @@ import numpy as np
 
 from pysages.colvars.orientation import RMSD_Kabsch
 
-base = np.loadtxt("./ci2_1.txt", dtype=str)[:, 6:9].astype(float)
-reference = np.loadtxt("./ci2_2.txt", dtype=str)[:, 6:9].astype(float)
+base = np.loadtxt("tests/ci2_1.txt", dtype=str)[:, 6:9].astype(float)
+reference = np.loadtxt("tests/ci2_2.txt", dtype=str)[:, 6:9].astype(float)
 indices = np.arange(len(base))
 rmsd_kabsch = RMSD_Kabsch(indices, reference)
 rmsd_calculated = rmsd_kabsch.function(base)
@@ -13,8 +13,8 @@ rmsd_expected = 11.7768
 if not np.allclose(rmsd_expected, rmsd_calculated):
     raise ValueError(f"Test Case PDB 1 failed: {rmsd_expected, rmsd_calculated}")
 
-base = np.loadtxt("./ci2_1t.txt", dtype=str)[:, 6:9].astype(float)
-reference = np.loadtxt("./ci2_1.txt", dtype=str)[:, 6:9].astype(float)
+base = np.loadtxt("tests/ci2_1t.txt", dtype=str)[:, 6:9].astype(float)
+reference = np.loadtxt("tests/ci2_1.txt", dtype=str)[:, 6:9].astype(float)
 rmsd_kabsch = RMSD_Kabsch(indices, reference)
 rmsd_calculated = rmsd_kabsch.function(base)
 rmsd_expected = 0.0
@@ -22,8 +22,8 @@ if not np.allclose(rmsd_expected, rmsd_calculated, atol=1e-3):
     raise ValueError(f"Test Case PDB 2 failed: {rmsd_expected, rmsd_calculated}")
 
 
-base = np.loadtxt("./ci2_2.txt", dtype=str)[:, 6:9].astype(float)
-reference = np.loadtxt("./ci2_12.txt", dtype=str)[:, 6:9].astype(float)
+base = np.loadtxt("tests/ci2_2.txt", dtype=str)[:, 6:9].astype(float)
+reference = np.loadtxt("tests/ci2_12.txt", dtype=str)[:, 6:9].astype(float)
 rmsd_kabsch = RMSD_Kabsch(indices, reference)
 rmsd_calculated = rmsd_kabsch.function(base)
 rmsd_expected = 15.422431
