aaa40ec8636b902bbbf604cda163e517bbd07896
yihengwuKP <yihengwu917@uchicago.edu>
2024-07-09 12:00:47 -0500

    implement checks for forces of Q

--- examples/openmm/cv_Q/check_force.py
+++ examples/openmm/cv_Q/check_force.py
@@ -4,44 +4,31 @@ import jax
 import numpy as np
 import openmm.app as app
 from jax import grad
+from openmm import unit
+from scipy.spatial import distance as sd
 
+from pysages.colvars.contacts import NativeContactFraction
 from pysages.colvars.orientation import ERMSD, RMSD
 
-pdb = app.PDBFile("../../../inputs/GAGA.box_0mM.pdb")
+contact_cutoff = 0.5  # nm
 
-C246_indices = []
-for i, res in enumerate(pdb.topology.residues()):
-    C246_residue = dict.fromkeys(["C2", "C4", "C6"])
-    for atom in res.atoms():
-        if atom.name in ["C2", "C4", "C6"]:
-            C246_residue[atom.name] = atom.index
-    C246_indices.append(C246_residue)
+pdb = app.PDBFile("../../inputs/GAGA.box_0mM.pdb")
+positions = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
+rna_indices = []
+for i, residue in enumerate(pdb.topology.residues()):
+    if residue.name in ["A", "U", "G", "C"]:
+        for atom in residue.atoms():
+            if atom.element.name != "hydrogen":
+                rna_indices.append(atom.index)
 
-# notice that the order of the indices for eRMSD is tricky!
-C246_indices_ordered = []
-for i, res in enumerate(pdb.topology.residues()):
-    if res.name in ["G", "A"]:
-        C246 = C246_indices[i]
-        C246_indices_ordered.extend((C246["C2"], C246["C6"], C246["C4"]))
-    elif res.name in ["U", "C"]:
-        C246 = C246_indices[i]
-        C246_indices_ordered.extend((C246["C2"], C246["C4"], C246["C6"]))
-
-reference = pdb.getPositions(asNumpy=True).astype("float")[np.asarray(C246_indices_ordered)]
-
-ermsd = ERMSD(C246_indices_ordered, reference)
-ermsd_grad = grad(ermsd.function)
-print(ermsd_grad(reference + np.random.random(reference.shape)))
-
-rmsd = RMSD(C246_indices_ordered, reference)
-rmsd_grad = grad(rmsd.function)
-print(rmsd_grad(reference + np.random.random(reference.shape)))
-
-
-def G(r, cutoff=2.4):
-    gamma = jax.numpy.pi / cutoff
-    end = jax.numpy.sin(gamma * r) * jax.numpy.heaviside(cutoff - r, np.zeros(r.shape))
-    return end[0]
-
-
-print(grad(G)(np.ones((10,))))
+rna_pos = positions.astype("float")[np.asarray(rna_indices)]
+contact_matrix = sd.squareform(sd.pdist(rna_pos)) < contact_cutoff
+contacts = np.transpose(np.nonzero(contact_matrix))
+rna_id_contacts = np.array([[rna_indices[i], rna_indices[j]] for i, j in contacts])
+indices = np.unique(rna_id_contacts)
+references = positions.astype("float")[np.asarray(indices)]
+ncf = NativeContactFraction(indices, rna_id_contacts, references)
+ncf_grad = grad(ncf.function)
+grad_hot = ncf_grad(references + np.random.random(references.shape))
+assert not np.any(np.isnan(grad_hot)), "force contains NaN values"
+print("checking for forces of Q passed!")
