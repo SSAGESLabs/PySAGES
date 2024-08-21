1a974c71f11bd019c93796b7fd3665b63a78745c
gustavor101 <gustavor101@gmail.com>
2024-08-20 20:47:29 -0500

    Add files via upload
    Add perpendicular projection as CV

--- pysages/colvars/funnels.py
+++ pysages/colvars/funnels.py
@@ -66,7 +66,7 @@ def kabsch(P0, Q0, weights):
 
 class Projection_on_Axis_mobile(CollectiveVariable):
     """
-    Use a reference to calculate the RMSD of a set of atoms.
+    Use a reference to calculate the projection on an axis of a set of atoms.
     The algorithm is based on https://doi.org/10.1002/jcc.20110.
 
     Parameters
@@ -147,3 +147,78 @@ def projection_mobile(
     norm = linalg.norm(vector)
     eje = vector / norm
     return np.dot(eje, lig_rot - A)
+
+
+class Perp_projection_on_Axis_mobile(CollectiveVariable):
+    """
+    Use a reference to calculate the perpendicular projection on an axis of a set of atoms.
+    The algorithm is based on https://doi.org/10.1002/jcc.20110.
+
+    Parameters
+    ----------
+    indices: list[int], list[tuple(int)]
+       Select atom groups via indices.
+
+    references: list[tuple(float)]
+       Cartesian coordinates of the reference position of the atoms in indices.
+       The coordinates must match the ones of the atoms used in indices.
+    weights_lig: list[tuple(float)]
+       weights of the mobile group of particles
+    weights_prot: list[tuple(float)]
+       weights of the reference group of particles
+    A: list[tuple(float)]
+       Initial vector of the axis
+    B: list[tuple(float)]
+       Final vector of the axis
+    box: list[tuple(float)]
+       Box vector of the axis (only works for orthorhombic boxes for now)
+    """
+
+    def __init__(
+        self,
+        indices,
+        references,
+        weights_lig=None,
+        weights_prot=None,
+        A=[0, 0, 0],
+        B=[0, 0, 1],
+        box=[1.0, 1.0, 1.0],
+    ):
+        super().__init__(indices, 3)
+        self.requires_box_unwrapping = True
+        self.references = np.asarray(references)
+        self.weights_lig = np.asarray(weights_lig) if weights_lig else None
+        self.weights_prot = np.asarray(weights_prot) if weights_prot else None
+        self.A = np.asarray(A)
+        self.B = np.asarray(B)
+        self.box = np.asarray(box)
+
+    @property
+    def function(self):
+        return lambda r1, r2, r3: perp_projection_mobile(
+            r1,
+            r2,
+            r3,
+            self.references,
+            self.weights_lig,
+            self.weights_prot,
+            self.A,
+            self.B,
+            self.box,
+        )
+
+
+def perp_projection_mobile(
+    ligand, backbone, pos_anchor, references, weights_lig, weights_prot, A, B, box
+):
+    ligand_distances = periodic(ligand - pos_anchor, box)
+    new_lig_pos = pos_anchor + ligand_distances
+    com_lig = center(new_lig_pos, weights_lig)
+    com_prot = center(backbone, weights_prot)
+    com_ref = center(references, weights_prot)
+    lig_rot = np.dot(com_lig - com_prot, kabsch(backbone, references, weights_prot)) + com_ref
+    vector = B - A
+    norm = linalg.norm(vector)
+    eje = vector / norm
+    perp = (lig_rot - A) - np.dot(eje, lig_rot - A) * eje
+    return linalg.norm(perp)
