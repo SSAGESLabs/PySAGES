14fb0371ced8c132cee5bee8fee8c1250b5a2613
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2024-12-03 14:02:52 -0600

    [skip ci] Add support for Qbox as backend

--- pysages/backends/__init__.py
+++ pysages/backends/__init__.py
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: MIT
 # See LICENSE.md and CONTRIBUTORS.md at https://github.com/SSAGESLabs/PySAGES
 
-from .contexts import JaxMDContext, JaxMDContextState  # noqa: E402, F401
+from .contexts import (  # noqa: E402, F401
+    JaxMDContext,
+    JaxMDContextState,
+    QboxContextGenerator,
+)
 from .core import SamplingContext, supported_backends  # noqa: E402, F401
