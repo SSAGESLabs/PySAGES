715187d089f9a3aa4d5d44e99a2d97dd40f7d1c4
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2024-12-03 13:16:28 -0600

    Move Contexts to a dedicated module

--- pysages/backends/__init__.py
+++ pysages/backends/__init__.py
@@ -1,9 +1,5 @@
 # SPDX-License-Identifier: MIT
 # See LICENSE.md and CONTRIBUTORS.md at https://github.com/SSAGESLabs/PySAGES
 
-from .core import (  # noqa: E402, F401
-    JaxMDContext,
-    JaxMDContextState,
-    SamplingContext,
-    supported_backends,
-)
+from .contexts import JaxMDContext, JaxMDContextState  # noqa: E402, F401
+from .core import SamplingContext, supported_backends  # noqa: E402, F401
