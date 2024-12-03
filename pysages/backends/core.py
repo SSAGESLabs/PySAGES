89a1f082b414105b429ba886fa17e5d4a7705040
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2024-12-03 13:16:28 -0600

    Move Contexts to a dedicated module

--- pysages/backends/core.py
+++ pysages/backends/core.py
@@ -3,58 +3,8 @@
 
 from importlib import import_module
 
-from pysages.typing import Any, Callable, JaxArray, NamedTuple, Optional
-
-JaxMDState = Any
-
-
-class JaxMDContextState(NamedTuple):
-    """
-    Provides an interface for the data structure returned by `JaxMDContext.init_fn` and
-    expected as the single argument of `JaxMDContext.step_fn`.
-
-    Arguments
-    ---------
-    state: JaxMDState
-        Holds the particle information and corresponds to the internal state of
-        `jax_md.simulate` methods.
-
-    extras: Optional[dict]
-        Additional arguments required by `JaxMDContext.step_fn`, these might include for
-        instance, the neighbor list or the time step.
-    """
-
-    state: JaxMDState
-    extras: Optional[dict]
-
-
-class JaxMDContext(NamedTuple):
-    """
-    Provides an interface for the data structure expects from `generate_context` for
-    `jax_md`-backed simulations.
-
-    Arguments
-    ---------
-    init_fn: Callable[..., JaxMDContextState]
-        Initilizes the `jax_md` state. Generally, this will be the `init_fn` of any
-        of the simulation routines in `jax_md` (or wrappers around these).
-
-    step_fn: Callable[..., JaxMDContextState]
-        Takes a state and advances a `jax_md` simulation by one step. Generally, this
-        will be the `apply_fn` of any of the simulation routines in `jax_md` (or wrappers
-        around these).
-
-    box: JaxArray
-        Affine transformation from a unit hypercube to the simulation box.
-
-    dt: float
-        Step size of the simulation.
-    """
-
-    init_fn: Callable[..., JaxMDContextState]
-    step_fn: Callable[..., JaxMDContextState]
-    box: JaxArray
-    dt: float
+from pysages.backends.contexts import JaxMDContext
+from pysages.typing import Callable, Optional
 
 
 class SamplingContext:
